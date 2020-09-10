3c2bc6f01032f8abc984587ed89697ec63817d09
EricksonLopes <ofc.erickson@gmail.com>
2020-09-10 09:03:12 -0300

    A área administrativa dos projetos Django e o arquivo admin.py

--- Dajngo_Project_1/core/models.py
+++ Dajngo_Project_1/core/models.py
@@ -7,8 +7,14 @@ class Produto(models.Model):
     estoque = models.IntegerField('Quantidade em Estoque')
     created_at = models.DateTimeField(auto_now_add=True)
 
+    def __str__(self):
+        return self.nome
+
 
 class Cliente(models.Model):
     nome = models.CharField('nome', max_length=100)
     sobrenome = models.CharField('sobrenome', max_length=100)
     email = models.EmailField('E-mail', max_length=100)
+
+    def __str__(self):
+        return self.nome + ' ' + self.sobrenome
