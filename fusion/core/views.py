166de156fe8cef1c004fa912dcc07c2eb531c729
EricksonLopes <ofc.erickson@gmail.com>
2020-09-13 16:03:15 -0300

    Apresentando dados nos templates de forma aleatória ('?')

--- fusion/core/views.py
+++ fusion/core/views.py
@@ -1,5 +1,14 @@
 from django.views.generic import TemplateView
+from .models import Servico, Funcionario
 
 
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def get_context_data(self, **kwargs):
+        context = super(IndexView, self).get_context_data(**kwargs)
+        # implementando dados iterando
+        context['servicos'] = Servico.objects.order_by('?')  # embaralhar
+        context['funcionario'] = Funcionario.objects.all()
+
+        return context
